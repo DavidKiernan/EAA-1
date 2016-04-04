0f487f270660305a4c07cace1896d7ca975c774c
Graham Healy <grahamh220@gmail.com>
2016-04-04 13:35:18 +0100

    Write up

--- Week9/script.R
+++ Week9/script.R
@@ -3,7 +3,10 @@ data<-read.table("mpstat-data-2016-04-04-12:32:16.dat", header=TRUE);
 
 #Get util time
 data.util<-(100 - data$X.idle);
+#Save PNG
+png("plog.png");
 plot(data.util, type="b", xlab="Time", ylab="CPU USR");
+dev.off();
 
 data.summary<-summary(data.util);
 data.sd<-sd(data.util);
